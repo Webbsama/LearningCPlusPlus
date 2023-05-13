b011983ba13349d391108ae64ba8d84a3949ef8b
Webbsama <web18016@byui.edu>
2023-05-13 17:28:24 -0600

    Finished writing variables. It ran right.

--- variables.cpp
+++ variables.cpp
@@ -1,3 +1,8 @@
+#include <iostream>
+// Calls compliler.
+int main()
+{
+
 /* This is what I learned about variables and how to use them */
 
 /* Create a variable using the following syntax*/
@@ -20,19 +25,19 @@ char firstInitial = 'S';
 /* String */
 /* Stores text */
 /* Needs stored in double quotes */
-string basicPhrase = "Hello World!";
+std::string basicPhrase = "Hello World!";
 
 /* Booleans */
 /* True or False */
 bool myBoolean = true;
 
 /* Learned how to display these variables */
-cout << basicPhrase <<
-    "I am Samantha." <<
-    "My favorite number is " << favNumber << 
-    "and my favorite decimal is " << favDecimal <<
-    ". " << "That will always be " << myBoolean <<
-// Should print:
-// Hello World! I am Samantha. 
-// My favorite number is 6 and my favorite decimal is 0.24. 
-// That will always be true. 
+std::cout << basicPhrase << " " <<
+"My initial is " << firstInitial << ". " <<
+"My favorite number is " << favNumber << ". " <<
+"My favorite decimal is " << favDecimal << ". " <<
+"This will alwys be " << myBoolean << ".";
+
+
+
+}
